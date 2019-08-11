1199ac4d6d54c3fc779f6a0410ad124d2edcfbb6
doms12 <20908320+doms12@users.noreply.github.com>
2019-08-11 14:52:55 +0100

    Create rgTFState.sh

---
+++ rgTFState.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RESOURCE_GROUP_NAME="terraformTFState"
+STORAGE_ACCOUNT_NAME="tfstate$RANDOM"
+CONTAINER_NAME="tfstate"
+ 
+# Create resource group
+#az group create --name $RESOURCE_GROUP_NAME --location uksouth
+ 
+# Create storage account
+az.cmd storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+  
+# Get storage account key
+ACCOUNT_KEY=$(az.cmd storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+ 
+# Create blob container
+az.cmd storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
